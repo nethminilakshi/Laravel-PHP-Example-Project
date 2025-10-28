1d06455955207fb61899152315740f23fe24aac3
Nethu <nilakshinethmi48@gmail.com>
2025-10-28 16:06:42 +0530

    pagination and seeders

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -3,14 +3,19 @@
         Job Listings
     </x-slot:heading>
 
-   <ul>
+  <div class="space-y-4">
     @foreach ($jobs as $job)
-        <li>
-           <a href="/jobs/{{$job['id']}}" class= "text-green-500 hover:underline">
+       
+           <a href="/jobs/{{$job['id']}}" class= "text-gray-500 hover:underline block px-4 border border-black-300 rounded-lg">
+            <div class= "font-bold text-blue-600">{{$job->employer->name}}</div>
         <strong>{{$job['title'] }}:</strong> Pays {{$job['salary']}} per year.
         </a> 
-    </li>
+   
     @endforeach
-    </ul>
+
+    <div>
+        {{ $jobs->links() }}
+    </div>
+  </div>
 
 </x-layout>
